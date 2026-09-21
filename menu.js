efc23ebc65929bbf49ebbd63609d3371515c4644
Rashid Anwer <rashidanwer2@gmail.com>
2026-09-21 23:09:39 +0530

    menu

--- menu.js
+++ menu.js
@@ -15,7 +15,7 @@ const LUQMA_CONFIG = {
 
   items: [
     {
-      id: "CB01",
+      id: "CDB01",
       name: "Chicken Dum Biryani",
       description: "Aromatic dum biryani served with raita and fresh salad",
       price: 220,
@@ -24,16 +24,7 @@ const LUQMA_CONFIG = {
       image: "assets/images/BiryaniMenu.jpg"
     },
     {
-      id: "MB01",
-      name: "Mutton Dum Biryani",
-      description: "Slow-cooked mutton dum biryani served with raita and salad.",
-      price: 280,
-      quantity: 10,
-      active: false,
-      image: "https://images.pexels.com/photos/16020573/pexels-photo-16020573.jpeg?auto=compress&cs=tinysrgb&w=1200&fit=crop"
-    },
-    {
-      id: "CK01",
+      id: "RCK01",
       name: "Reshami Chicken Kebab",
       description: "Juicy homemade chicken kebabs, freshly prepared in small batches.",
       price: 180,
@@ -42,7 +33,7 @@ const LUQMA_CONFIG = {
       image: "https://images.pexels.com/photos/6089832/pexels-photo-6089832.jpeg?auto=compress&cs=tinysrgb&w=1200&fit=crop"
     },
     {
-      id: "MK01",
+      id: "CSK01",
       name: "Chicken Seekh Kebab",
       description: "Tender spiced chicken kebabs with a smoky finish.",
       price: 220,
@@ -51,7 +42,34 @@ const LUQMA_CONFIG = {
       image: "https://images.pexels.com/photos/15058960/pexels-photo-15058960.jpeg?auto=compress&cs=tinysrgb&w=1200&fit=crop"
     },
     {
-      id: "MC01",
+      id: "MDB01",
+      name: "Mutton Dum Biryani",
+      description: "Slow-cooked mutton dum biryani served with raita and salad.",
+      price: 280,
+      quantity: 10,
+      active: false,
+      image: "https://images.pexels.com/photos/16020573/pexels-photo-16020573.jpeg?auto=compress&cs=tinysrgb&w=1200&fit=crop"
+    },
+    {
+      id: "RMK01",
+      name: "Reshami Mutton Kebab",
+      description: "Juicy homemade mutton kebabs, freshly prepared in small batches.",
+      price: 180,
+      quantity: 8,
+      active: false,
+      image: "https://images.pexels.com/photos/6089832/pexels-photo-6089832.jpeg?auto=compress&cs=tinysrgb&w=1200&fit=crop"
+    },
+    {
+      id: "MSK01",
+      name: "Mutton Seekh Kebab",
+      description: "Tender spiced mutton kebabs with a smoky finish.",
+      price: 220,
+      quantity: 6,
+      active: false,
+      image: "https://images.pexels.com/photos/15058960/pexels-photo-15058960.jpeg?auto=compress&cs=tinysrgb&w=1200&fit=crop"
+    },
+    {
+      id: "MCP01",
       name: "Malai Chicken + Paratha",
       description: "Creamy malai chicken served with soft paratha and salad.",
       price: 240,
